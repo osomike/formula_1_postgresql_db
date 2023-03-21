a93b04d605e89ace29e267680017a19949b7e3df
Oscar Mike <mike.claure@gmail.com>
2023-03-21 11:59:50 +0100

    Add answers for the missing questions

--- questions/answers.sql
+++ questions/answers.sql
@@ -146,3 +146,108 @@ FROM results_target_complete
 WHERE position IN (1, 2, 3)
 GROUP BY forename, surname, name
 ORDER BY  count(position) DESC
+
+------------------------------------------------------------------------------------------------------------------------
+-- 4 Which teams had the most accidents during the season of 2022? An ordered table would be easier to read.
+WITH races_target AS (
+    SELECT
+        raceid,
+        year
+    FROM bronze.races
+    WHERE races.year = 2022
+    ),
+
+status_target AS (
+    SELECT *
+    FROM bronze.status
+    WHERE status = 'Accident'
+    ),
+
+results_target AS (
+    SELECT
+        races_target.raceid,
+        races_target.year,
+        results.driverid,
+        results.constructorid,
+        results.position
+        results.statusid
+    FROM
+        races_target JOIN bronze.results ON races_target.raceid = bronze.results.raceid
+    WHERE
+        results.statusid = status_target
+    )
+
+SELECT *
+FROM results_target
+
+------------------------------------------------------------------------------------------------------------------------
+-- 5 Who won the F1 drivers’ tournament and the constructors’ tournament during the season of 2022?
+-- For the constructors tournament
+WITH races_target AS (
+    SELECT
+        raceid,
+        year
+    FROM bronze.races
+    WHERE date = (SELECT MAX(date) FROM bronze.races WHERE races.year = 2022)
+    ),
+
+constructors_standings_target AS (
+    SELECT
+		races_target.raceid,
+		races_target.year,
+		constructor_standings.constructorid,
+		constructor_standings.points
+    FROM
+        races_target JOIN bronze.constructor_standings ON constructor_standings.raceid = races_target.raceid
+),
+
+contructor_standings_target_with_name AS (
+	SELECT
+		constructors_standings_target.year,
+		constructors_standings_target.points,
+		constructors.name,
+		constructors.nationality,
+		constructors_standings_target.constructorid
+	FROM constructors_standings_target JOIN bronze.constructors ON constructors_standings_target.constructorid = constructors.constructorid
+
+)
+
+SELECT *
+FROM contructor_standings_target_with_name
+ORDER BY points DESC
+
+
+-- For the drivers tournament
+WITH races_target AS (
+    SELECT
+        raceid,
+        year
+    FROM bronze.races
+    WHERE date = (SELECT MAX(date) FROM bronze.races WHERE races.year = 2022)
+    ),
+
+driver_standings_target AS (
+    SELECT
+		races_target.raceid,
+		races_target.year,
+		driver_standings.driverid,
+		driver_standings.points
+    FROM
+        races_target JOIN bronze.driver_standings ON driver_standings.raceid = races_target.raceid
+),
+
+driver_standings_target_with_name AS (
+	SELECT
+		driver_standings_target.year,
+		driver_standings_target.points,
+		drivers.forename,
+		drivers.surname,
+		drivers.nationality,
+		driver_standings_target.driverid
+	FROM driver_standings_target JOIN bronze.drivers ON driver_standings_target.driverid = drivers.driverid
+
+)
+
+SELECT *
+FROM driver_standings_target_with_name
+ORDER BY points DESC
